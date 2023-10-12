2df227bb91b17f159c330082e4cc56ca12fc5f20
Mike Grammes <mike.grammes@gmail.com>
2023-10-11 23:24:28 -0400

    remove unused font theme

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -3,18 +3,6 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-"rebelot/kanagawa.nvim",  -- neorg needs a colorscheme with treesitter support
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-      highlight = { enable = true },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-    end,
-  },
-  {
     "nvim-neorg/neorg",
     build = ":Neorg sync-parsers",
     dependencies = { "nvim-lua/plenary.nvim" },
@@ -38,5 +26,4 @@ return {
       vim.wo.conceallevel = 2
     end,
   }
-}
 
