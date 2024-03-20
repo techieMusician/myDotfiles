0d0986a7556048e2c1038276120768fc68a4995a
Mike Grammes <mike.grammes@gmail.com>
2024-03-20 00:41:55 -0400

    add telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -23,7 +23,9 @@ vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
 { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-{ "folke/which-key.nvim"}
+{ "folke/which-key.nvim"},
+{ "nvim-telescope/telescope.nvim", branch = '0.1.x',
+    dependencies = { "nvim-lua/plenary.nvim" }}
 }
 local opts = {}
 
@@ -32,5 +34,12 @@ require("lazy").setup(plugins, opts)
 require("catppuccin").setup({
   flavour = "mocha",
 })
+
+-- setup keybindings for Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 vim.cmd.colorscheme "catppuccin"
