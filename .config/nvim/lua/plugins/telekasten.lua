74f0abf1d88a2bd2c8b0e5478a07f29662a186c9
Mike Grammes <mike.grammes@gmail.com>
2024-03-22 01:42:33 -0400

    add keybindings to telekasten

--- .config/nvim/lua/plugins/telekasten.lua
+++ .config/nvim/lua/plugins/telekasten.lua
@@ -7,5 +7,21 @@ return {
       dailies = vim.fn.expand("~/zettelkasten/dailies"),
       weeklies = vim.fn.expand("~/zettelkasten/weeklies"),
     })
+    -- Most used functions
+    vim.keymap.set("n", "<leader>zf", "<cmd>Telekasten find_notes<CR>")
+    vim.keymap.set("n", "<leader>zg", "<cmd>Telekasten search_notes<CR>")
+    vim.keymap.set("n", "<leader>zd", "<cmd>Telekasten goto_today<CR>")
+    vim.keymap.set("n", "<leader>zz", "<cmd>Telekasten follow_link<CR>")
+    vim.keymap.set("n", "<leader>zn", "<cmd>Telekasten new_note<CR>")
+    vim.keymap.set("n", "<leader>zc", "<cmd>Telekasten show_calendar<CR>")
+    vim.keymap.set("n", "<leader>zb", "<cmd>Telekasten show_backlinks<CR>")
+    vim.keymap.set("n", "<leader>zI", "<cmd>Telekasten insert_img_link<CR>")
+    vim.keymap.set({"n", "i"}, "<C-t>", "<cmd>Telekasten toggle_todo<CR>")
+    local wk = require("which-key")
+    wk.register({
+      ["<leader>z"] = {
+        name = "Zettelkasten",
+      },
+    })
   end,
 }
