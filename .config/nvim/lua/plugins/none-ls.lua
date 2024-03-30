ee61afa1e7d763504983e4eddd0610ea6b7009ff
Mike Grammes <mike.grammes@gmail.com>
2024-03-30 00:16:40 -0400

    Feature: Add support for clang_format

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,13 +1,14 @@
 -- package is none-ls but replaces and uses null-ls
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-      },
-    })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format using Code Style" })
-  end,
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.clang_format,
+			},
+		})
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format using Code Style" })
+	end,
 }
