8944abcafce88d3cac526d8fa1c0ddb2c48d5fa3
Mike Grammes <mike.grammes@gmail.com>
2024-04-29 01:01:12 -0400

    Add clipboard copy and past to nvim

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -7,4 +7,5 @@ vim.cmd("set number")
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-
+-- Set clipboard to unamed so I can copy and paste with outside programes
+vim.cmd("set clipboard=unnamed")
