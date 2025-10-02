ef41ea7c802bfd2da03dd69f6f324499de569c16
Mike Grammes <mike.grammes@gmail.com>
2025-10-02 01:55:49 -0400

    add mason support for lsp config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,8 @@
 return {
-
+  -- Installs language servers, linters, and formatters
+  'williamboman/mason.nvim',
+  -- Bridges mason.nvim with nvim-lspconfig
+  'williamboman/mason-lspconfig.nvim',
+  -- Provides easy LSP configurations for many servers
+  'neovim/nvim-lspconfig',
 }
