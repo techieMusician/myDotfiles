552122f19eae84fea38097989ab91d5e13c33c08
Mike Grammes <mike.grammes@gmail.com>
2026-05-11 14:32:36 -0400

    add markdownflow for control of md files

---
+++ .config/nvim/lua/plugins/mkdownflow.lua
@@ -0,0 +1,9 @@
+return {
+        'jakewvincent/mkdnflow.nvim',
+        ft = { 'markdown', 'rmd' },  -- Add custom filetypes here if configured
+        config = function()
+            require('mkdnflow').setup({
+                -- Your config
+            })
+        end
+    }
