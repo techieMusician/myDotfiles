1b4b215a8237a2cdc5d1a6d3c2be451f48477544
Mike Grammes <mike.grammes@gmail.com>
2026-05-11 14:31:47 -0400

    update treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   {  "nvim-treesitter/nvim-treesitter",
   branch = "main",
+  lazy = false,
   build = ":TSUpdate",
   config = function()
     local config = require("nvim-treesitter")
@@ -8,7 +9,6 @@ return {
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
-      ignore_install = { "latex" },
       })
   end,
   },
