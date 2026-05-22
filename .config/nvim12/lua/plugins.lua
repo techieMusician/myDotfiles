2ab7f2c8c8621a06cf8e0bc1af11ca0041638972
Mike Grammes <mike.grammes@gmail.com>
2026-05-22 14:09:49 -0400

    first new config

---
+++ .config/nvim12/lua/plugins.lua
@@ -0,0 +1,10 @@
+
+vim.pack.add({
+    { src = "https://github.com/mason-org/mason.nvim" },
+})
+
+require("mason").setup({})
+
+vim.pack.add({
+	{ src = "https://github.com/folke/which-key.nvim" },
+})
