9bb6212d6c8d9f9a2e33b03cecf75d8f7b93314d
Mike Grammes <mike.grammes@gmail.com>
2026-05-23 05:10:58 -0400

    add zen-mode

---
+++ .config/nvim12/plugin/zen-mode.lua
@@ -0,0 +1,12 @@
+vim.pack.add({
+    "https://github.com/folke/zen-mode.nvim"
+})
+
+require('zen-mode').setup({
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    window = {
+      width = 80,
+  }
+})
