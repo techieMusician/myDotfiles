5b899b22e79db11bd1bb0a1134dc021ecbcef2d5
Mike Grammes <mike.grammes@gmail.com>
2026-05-23 04:55:56 -0400

    add lonelog and my fork mythicOracle

---
+++ .config/nvim12/plugin/lonelog.lua
@@ -0,0 +1,44 @@
+vim.pack.add({
+    "https://github.com/Django0033/lonelog.nvim",
+    "https://github.com/TechieMusician/mythic.nvim"
+})
+
+require("lonelog").setup({
+-- Keybindings
+    keymaps = {
+      oracle = "<leader>lo",
+      dice = "<leader>ldr",
+      tags = "<leader>lt",
+      scenes = "<leader>ls",
+      chaos = "<leader>lC",
+      d4 = "<leader>ld4",
+      d6 = "<leader>ld6",
+      d8 = "<leader>ld8",
+      d10 = "<leader>lda",
+      d12 = "<leader>ldb",
+      d20 = "<leader>ldw",
+      d100 = "<leader>ldc",
+    },
+
+    use_telescope = auto,  -- Set to true or "auto" to use Telescope picker
+    sidebar = { width = 50 },  -- Sidebar width when Telescope is disabled
+    -- Floating window settings
+    float = {
+      border = "rounded",
+      height = 0.4,
+      width = 0.6,
+    },
+
+  -- Oracle settings
+    oracle = {
+      default_table = "fate",
+      persist_chaos = true,      -- Persist Mythic chaos factor to file
+      chaos_file = "chaos_factor.json",  -- Chaos factor file
+    },
+
+  -- Dice settings
+    dice = {
+      max_dice = 100,
+      max_sides = 1000,
+    },
+  })
