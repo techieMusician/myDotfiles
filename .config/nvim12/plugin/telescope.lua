09925fc0a62993cf6f39ecc4dcd2bf7404559f5a
Mike Grammes <mike.grammes@gmail.com>
2026-05-23 22:46:32 -0400

    fix telescope ui

--- .config/nvim12/plugin/telescope.lua
+++ .config/nvim12/plugin/telescope.lua
@@ -11,6 +11,7 @@ require("telescope").setup({
         },
     },
 })
+require("telescope").load_extension("ui-select")
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
